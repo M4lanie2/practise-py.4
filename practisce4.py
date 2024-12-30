7ff5a8baf66da60c404b254025cbce60d55f44cd
M4lanie2 <ekwuemejohn@gmail.com>
2024-12-30 19:21:52 +0000

    Add files via upload

---
+++ practisce4.py
@@ -0,0 +1,15 @@
+Good="Goodjob"
+print(type("Good"))
+#string datatype
+Champ="you did well"
+print(type("Champ"))
+proud="we are proud"
+print(type(proud))
+keep="keep up the good job"
+awsome="you are awsome"
+#floatpoint datatype
+print(type(awsome))
+amazingstar="keep up the amazing work star"
+print=type(amazingstar)
+print("enter what you have gotton congratulated for")
+
